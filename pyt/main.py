2c624cc4a0e2c47cb6d5543a98dc343a706b6c76
Muhammad A Alkarouri <malkarouri@tabahuth.com>
2016-05-19 19:52:24 +0400

    Implement a cliff app for main

--- pyt/main.py
+++ pyt/main.py
@@ -1,3 +1,24 @@
-#
-def main():
-    print "Hello, world from Pyt"
+import sys
+
+from cliff.app import App
+from cliff.commandmanager import CommandManager
+
+class MgmtApp(App):
+    def __init__(self):
+        super(MgmtApp, self).__init__(
+            description='Manage Environments',
+            version='0.1',
+            command_manager=CommandManager('mgmt.main'),
+            deferred_help=True,
+        )
+    def initialize_app(self, argv):
+        pass
+    def prepare_to_run_command(self, cmd):
+        pass
+    def clean_up(self, cmd, result, err):
+        if err:
+            self.LOG.debug("Error: {}".format(err))
+
+def main(argv=sys.argv[1:]):
+    myapp = MgmtApp()
+    return myapp.run(argv)
