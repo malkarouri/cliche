15362903dcd080adf83017437ef2676cda09bbe9
Muhammad A Alkarouri <malkarouri@tabahuth.com>
2016-05-22 17:16:10 +0400

    Have the prompt be the environment name

--- cliche/main.py
+++ cliche/main.py
@@ -17,6 +17,23 @@ class MgmtApp(App):
         self.config_path = "{}.cfg".format(self.app_name)
         self.config = ConfigParser.ConfigParser()
         self.config.read(self.config_path)
+    def interact(self):
+        # Defer importing .interactive as cmd2 is a slow import
+        from cliff.interactive import InteractiveApp
+
+        if self.interactive_app_factory is None:
+            self.interactive_app_factory = InteractiveApp
+            self.interpreter = self.interactive_app_factory(self,
+                                                            self.command_manager,
+                                                            self.stdin,
+                                                            self.stdout,
+            )
+        self.initialize_interactive()
+        self.interpreter.cmdloop()
+        return 0
+    def initialize_interactive(self):
+        prompt = self.config.get('environment', 'name') if self.config.has_option('environment', 'name') else ""
+        self.interpreter.prompt = "[{}]: ".format(prompt)
     def initialize_app(self, argv):
         pass
     def prepare_to_run_command(self, cmd):
